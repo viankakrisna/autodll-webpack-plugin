28560ba8033fa9ded31aa3a3f618e78b5ef13994
Ade Viankakrisna Fadlil <viankakrisna@gmail.com>
2017-07-11 03:22:20 +0700

    add sourceMethod setting

--- src/createHash.js
+++ src/createHash.js
@@ -1,41 +1,56 @@
 import crypto from 'crypto';
 import path from 'path';
+import get from 'lodash/get';
 import fs from './utils/fs';
 import { cacheDir } from './paths';
 
-const getContents = watchPath => {
-  try {
-    if (fs.existsSync(watchPath)) {
-      if (fs.lstatSync(watchPath).isDirectory()) {
-        if (watchPath.startsWith(cacheDir)) {
-          return '';
+const getSources = (watchPaths, sourceMethod) => {
+  const getSource = watchPath => {
+    try {
+      if (fs.existsSync(watchPath)) {
+        if (fs.lstatSync(watchPath).isDirectory()) {
+          if (watchPath.startsWith(cacheDir)) {
+            return '';
+          }
+          return fs
+            .readdirSync(watchPath)
+            .map(p => getSource(path.join(watchPath, p)))
+            .join('');
+        } else {
+          return sourceMethod(watchPath);
         }
-        return fs
-          .readdirSync(watchPath)
-          .map(p => getContents(path.join(watchPath, p)))
-          .join('');
-      } else {
-        return fs.readFileSync(watchPath, 'utf-8');
       }
+    } catch (ignored) {
+      // Just fallback to empty string below
     }
     return '';
-  } catch (e) {
-    //Failed to read file, fallback to string
-    return '';
+  };
+  return watchPaths.map(getSource).join('');
+};
+
+const getSourceMethod = key => {
+  switch (key) {
+  case 'content':
+    return filePath => fs.readFileSync(filePath, 'utf-8');
+  case 'mtime':
+  default:
+    return filePath => `${filePath}_${fs.statSync(filePath).mtime}`;
   }
 };
 
 const createHash = settings => {
   const hash = crypto.createHash('md5');
+  const watchPaths = get(settings, 'watch.paths', settings.watch);
   const settingsJSON = JSON.stringify(settings);
 
   hash.update(settingsJSON);
 
-  if (Array.isArray(settings.watch)) {
-    hash.update(settings.watch.map(getContents).join(''));
+  if (Array.isArray(watchPaths)) {
+    const sourceMethodKey = get(settings, 'watch.sourceMethod', 'mtime');
+    hash.update(getSources(watchPaths, getSourceMethod(sourceMethodKey)));
   }
 
-  return [settings.env, settings.id, hash.digest('hex')].join('_');
+  return `${settings.env}_${settings.id}_${hash.digest('hex')}`;
 };
 
 export default createHash;
